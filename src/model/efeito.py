97c7cf470352795c3f9eb250681b2cb65e2917bd
Vinicius <vini.pessoa7@gmail.com>
2021-04-02 16:54:14 -0400

    feat: efeito.py: Estrutura inicial

---
+++ src/model/efeito.py
@@ -0,0 +1,35 @@
+class Efeito:
+    """
+    Representa os efeitos que o usuário pode utilizar em seu microfone para alterar sua voz.
+
+    Atributos
+    ---------
+    id_efeito : int
+        Identificador único de cada efeito.
+    titulo : String
+        Título do efeito, para que o usuário possa identificá-lo.
+    """
+
+    def __init__(self, id_efeito, titulo):
+        self.id_efeito = id_efeito
+        self.titulo = titulo
+    
+    def ativar(self):
+        """
+        Ativa o efeito no microfone do usuário.
+        """
+        # IMPLEMENTAÇÂO #
+        print('ativar()')
+
+    def desativar(self):
+        """
+        Desativa o efeito, caso esteja ativo.
+        """
+        # IMPLEMENTAÇÂO #
+        print('desativar()')
+
+if __name__ == '__main__':
+    efeito = Efeito(1, 'Echo')
+
+    efeito.ativar()
+    efeito.desativar() 
