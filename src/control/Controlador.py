106b065e2f503158ace8a92061c671c3bfab14ab
Vinicius <vini.pessoa7@gmail.com>
2021-04-06 14:59:09 -0400

    importa e instancia o soundboard no controlador

--- src/control/Controlador.py
+++ src/control/Controlador.py
@@ -1,3 +1,5 @@
+from Soundboard import Soundboard
+
 class Controlador:
     """
     Classe controladora do sistema. Responsável por interagir
@@ -9,7 +11,9 @@ class Controlador:
         Volume que o áudio será reproduzido no microfone do usuário.b(O padrão é 100)
     volume_retorno : int
         Volume que o usuário escutará o audio em seu dispositivo de saída (Fone/Caixa de som). (O padrão é 100)
-
+    soundboard : Soundboard
+        Instância da classe Soundboard, responsável por gerenciar os sons do usuário.
+    
     Métodos
     -------
 
@@ -28,6 +32,7 @@ class Controlador:
         """
         self.volume_saida = v_saida
         self.volume_retorno = v_retorno
+        self.soundboard = Soundboard()
         # TODO #1: INSTANCIA DA PAGINA PRINCIPAL
 
         # DEBUG
@@ -42,3 +47,5 @@ class Controlador:
 if __name__ == '__main__':
     # Instância do controlador
     controlador = Controlador()
+
+    controlador.soundboard.main()
