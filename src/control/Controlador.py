e511f9512dd831201c660c6ecb0cb96d12317bbd
Vinicius <vini.pessoa7@gmail.com>
2021-04-06 13:27:32 -0400

    feat: relacionamento das classes

--- src/control/Controlador.py
+++ src/control/Controlador.py
@@ -9,6 +9,10 @@ class Controlador:
         Volume que o áudio será reproduzido no microfone do usuário.b(O padrão é 100)
     volume_retorno : int
         Volume que o usuário escutará o audio em seu dispositivo de saída (Fone/Caixa de som). (O padrão é 100)
+    sons : list 
+        Lista do caminho dos sons no computador.
+    efeitos : list
+        Lista de efeitos. 
 
     Métodos
     -------
@@ -36,6 +40,9 @@ class Controlador:
         """
         self.volume_saida = v_saida
         self.volume_retorno = v_retorno
+        self.sons    = []
+        self.efeitos = []
+        # TODO #1: INSTANCIA DA PAGINA PRINCIPAL
 
         # DEBUG
         print('volume_saida = ', self.volume_saida)
