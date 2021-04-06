dca651198fcd110d032f87b802cf3b46a0a99454
Vinicius <vini.pessoa7@gmail.com>
2021-04-06 14:51:59 -0400

    feat: modularização controlador -> soundboard

--- src/control/Controlador.py
+++ src/control/Controlador.py
@@ -9,22 +9,10 @@ class Controlador:
         Volume que o áudio será reproduzido no microfone do usuário.b(O padrão é 100)
     volume_retorno : int
         Volume que o usuário escutará o audio em seu dispositivo de saída (Fone/Caixa de som). (O padrão é 100)
-    sons : list 
-        Lista do caminho dos sons no computador.
-    efeitos : list
-        Lista de efeitos. 
 
     Métodos
     -------
-    adiciona_som() : void
-        Adiciona um áudio do computador, selecionado pelo usuário, e adiciona 
-        na lista de áudios do programa.
-    remove_som() : void
-        Remove um áudio da lista de sons do programa.
-    toca_som(id) : void
-        Reproduz um áudio, de acordo com o id fornecido.
-    para_som() : void
-        Para a reprodução do áudio em execução, se houver um.
+
     """
 
     def __init__(self, v_saida=100, v_retorno=100):
@@ -40,8 +28,6 @@ class Controlador:
         """
         self.volume_saida = v_saida
         self.volume_retorno = v_retorno
-        self.sons    = []
-        self.efeitos = []
         # TODO #1: INSTANCIA DA PAGINA PRINCIPAL
 
         # DEBUG
@@ -49,45 +35,10 @@ class Controlador:
         print('volume_retorno = ', self.volume_retorno)
 
     # Métodos #
-    def adiciona_som(self):
-        """
-        Adiciona um áudio do computador, selecionado pelo usuário, e adiciona 
-        na lista de áudios do programa.
-        """
-        print('adicionar_som()')
-
-    def remove_som(self):
-        """
-        Remove um áudio da lista de sons do programa.
-        """
-        print('remover_som()')
-
-    def toca_som(self, id):
-        """
-        Reproduz um áudio, de acordo com o id fornecido.
-
-        Parâmetros
-        ----------
-        id : int
-            Representa o ID do som a ser reproduzido. 
-        """
-
-        print('tocar_som(%d)' % id)
-
-    def para_som(self):
-        """
-        Para a reprodução do áudio em execução, se houver um.
-        """
-        print('parar_som()')
+    
 
 # Main #
 # Usada pra testar os métodos da classe
 if __name__ == '__main__':
     # Instância do controlador
     controlador = Controlador()
-    
-    # Teste dos métodos do controlador
-    controlador.adiciona_som()
-    controlador.remove_som()
-    controlador.toca_som(10)
-    controlador.para_som()
---
+++ src/control/Soundboard.py
@@ -0,0 +1,75 @@
+class Soundboard:
+    """
+    Classe gerenciadora dos sons. Responsável por 
+    ler, armazenar, reproduzir, interromper e remover os sons.
+    
+    Atributos
+    ---------
+    sons : list 
+        Lista do caminho dos sons no computador.
+    efeitos : list
+        Lista de efeitos. 
+
+    Métodos
+    -------
+    adiciona_som() : void
+        Adiciona um áudio do computador, selecionado pelo usuário, e adiciona 
+        na lista de áudios do programa.
+    remove_som() : void
+        Remove um áudiso da lista de sons do programa.
+    toca_som(id) : void
+        Reproduz um áudio, de acordo com o id fornecido.
+    para_som() : void
+        Para a reprodução do áudio em execução, se houver um.
+    """
+
+    def __init__(self):
+        """
+        Construtor da classe.
+        """    
+        self.sons    = []
+        self.efeitos = []
+
+    # Métodos #
+    def adiciona_som(self):
+        """
+        Adiciona um áudio do computador, selecionado pelo usuário, e adiciona 
+        na lista de áudios do programa.
+        """
+        print('adicionar_som()')
+
+    def remove_som(self):
+        """
+        Remove um áudio da lista de sons do programa.
+        """
+        print('remover_som()')
+
+    def toca_som(self, id):
+        """
+        Reproduz um áudio, de acordo com o id fornecido.
+
+        Parâmetros
+        ----------
+        id : int
+            Representa o ID do som a ser reproduzido. 
+        """
+
+        print('tocar_som(%d)' % id)
+
+    def para_som(self):
+        """
+        Para a reprodução do áudio em execução, se houver um.
+        """
+        print('parar_som()')
+
+# Main #
+# Usada pra testar os métodos da classe
+if __name__ == '__main__':
+    # Instância do controlador
+    soundboard = Soundboard()
+    
+    # Teste dos métodos do controlador
+    soundboard.adiciona_som()
+    soundboard.remove_som()
+    soundboard.toca_som(10)
+    soundboard.para_som()
