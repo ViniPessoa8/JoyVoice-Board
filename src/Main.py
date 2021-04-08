0326f3aec07da2e10ac94e2005a3dfd06e39ec78
Vinicius <vini.pessoa7@gmail.com>
2021-04-08 10:18:52 -0400

    fix: função com nome errado

--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,7 @@ from control.Controlador import Controlador
 class Main:
     def __init__(self):
         self.controlador = Controlador()
-        self.controlador.soundboard.carregar_sons()
+        self.controlador.soundboard.carrega_sons()
 
 if __name__ == '__main__':
     main = Main()
