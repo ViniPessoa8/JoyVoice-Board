9cd1c9abce72d8d349075e5df6166181f998dbf2
Vinicius <vini.pessoa7@gmail.com>
2021-04-09 14:37:21 -0400

    fix: teste desnecessário

--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,6 @@ from control.Controlador import Controlador
 class Main:
     def __init__(self):
         self.controlador = Controlador()
-        self.controlador.soundboard.carrega_sons()
 
 if __name__ == '__main__':
     main = Main()
