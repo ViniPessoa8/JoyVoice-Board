760dee72a2f7126594f83093ce5c641f2379806e
Vinicius <vini.pessoa7@gmail.com>
2021-04-22 10:55:57 -0400

    docs (Tests): documenta métodos

--- src/util/Tests.py
+++ src/util/Tests.py
@@ -8,12 +8,18 @@ teste_toca_som() : void
 teste_para_som() : void
     Reproduz o primeiro áudio da lista de sons e, depois de 1 segundo, interrompe-o.
 teste_remove_som() : void
-    Remove o 
+    Remove o primeiro som da lista de sons, removendo-o também do arquivo sons.json
 """
 def teste_toca_som(self): 
+    """
+    Reproduz o primeiro audio da lista de sons.
+    """
     self.controlador.soundboard.toca_som(0)
 
 def teste_para_som(self):
+    """
+    Reproduz o primeiro áudio da lista de sons e, depois de 1 segundo, interrompe-o.
+    """
     # Reproduz o som
     self.controlador.soundboard.toca_som(0)
     print('Esperando 1 segundo... ')
@@ -23,5 +29,8 @@ def teste_para_som(self):
     self.controlador.soundboard.para_som()
 
 def teste_remove_som(self):
+    """
+    Remove o primeiro som da lista de sons, removendo-o também do arquivo sons.json
+    """
     self.controlador.soundboard.remove_som('com - Ratinho vinheta')
 
